f17ec3948907f37e1cb3b614f0d11667a60d84a5
Hash2000 <infinite_2000@mail.ru>
2025-02-11 19:10:46 +0300

    StockExchangeCity.UI для MAUI

--- StockExchangeCity.Game.Tools.MapEditor/MauiProgram.cs
+++ StockExchangeCity.Game.Tools.MapEditor/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 
 namespace StockExchangeCity.Game.Tools.MapEditor
 {
@@ -9,6 +10,7 @@ namespace StockExchangeCity.Game.Tools.MapEditor
 			var builder = MauiApp.CreateBuilder();
 			builder
 				.UseMauiApp<App>()
+				.UseSkiaSharp()
 				.ConfigureFonts(fonts =>
 				{
 					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
