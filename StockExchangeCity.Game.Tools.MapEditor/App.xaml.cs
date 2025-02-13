7081d9883bd9c5f67aa89289b886598fef8473ef
Hash2000 <infinite_2000@mail.ru>
2025-02-13 09:25:59 +0300

    рабочая версия StockExchangeCity.Game.Tools.MapEditor net MAUI

---
+++ StockExchangeCity.Game.Tools.MapEditor/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace StockExchangeCity.Game.Tools.MapEditor
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
